a2286c5dbb786ce387ebdf3f20d3986ead647fc2
event <event@2014-sk-04.games-academy.de>
2016-04-29 15:29:26 +0200

    Camera and Player Movement

---
+++ JourneyOfBalls/Assets/CameraSwitch.js
@@ -0,0 +1,16 @@
+﻿     var camer_main :  Main Camera ;
+      var camer_top :    Camera ;
+      function Start()
+                  {
+        camer_main.enabled  = true;
+        camer_top.enabled = false;
+                  }
+       function OnTriggerEnter(Collider cam_trigger_1)
+                 {
+                   
+                   if(other.gameObject.name == "Cube")
+                                   {
+                             camer_main.enabled  = false;
+                             camer_top.enabled = true;
+                                }
+                 }
--- JourneyOfBalls/Assets/PlayerMovement.js
+++ JourneyOfBalls/Assets/PlayerMovement.js
@@ -9,7 +9,7 @@ function Update()
 	var controller : CharacterController = GetComponent.<CharacterController>();
 	if (controller.isGrounded) 
 	{
-		moveDirection = Vector3(-Input.GetAxis("Horizontal"), 0,
+		moveDirection = Vector3(Input.GetAxis("Horizontal"), 0,
 		                        -Input.GetAxis("Vertical"));
 		moveDirection = transform.TransformDirection(moveDirection);
 		moveDirection *= speed;
